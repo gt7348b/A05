5fe5f2c33c7c3fa4b7139a29bfc76eab1c26d57f
John Crocker <johnniecrocker@yahoo.com>
2014-09-30 13:12:10 -0400

    added js variables

---
+++ main.js
@@ -0,0 +1,11 @@
+var value1,
+    value2,
+    answer;
+
+
+document.getElementById('calculateBtn').onclick = function() {
+
+  answer = document.getElementById('num_one').value
+            +
+           document.getElementById('num_two').value;
+};
