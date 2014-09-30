cf07d11737366fd622aade4b80eac0135b5570a2
John Crocker <johnniecrocker@yahoo.com>
2014-09-30 13:33:19 -0400

    got values to load when clicked

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ var value1,
     value2,
     answer;
 
+var answer = function (name) {
+  text = "South Fulton " + name;
+  document.getElementById('greetingArea').innerHTML = greeting;
+}
+
 
 document.getElementById('calculateBtn').onclick = function() {
 
-  answer = document.getElementById('num_one').value
-            +
-           document.getElementById('num_two').value;
+value1 = Number(document.getElementById('num_one').value);
+
+value2 = Number(document.getElementById('num_two').value);
+
 };
